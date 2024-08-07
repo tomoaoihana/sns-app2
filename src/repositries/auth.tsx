b5f00b87a4a25540872773ec9b9bed8c75aadd3f
tomoaoihana <tamu.kusanishi@gmail.com>
2024-08-07 11:27:26 +0900

    会員登録画面作成

---
+++ src/repositries/auth.tsx
@@ -0,0 +1,26 @@
+import { User as SupabaseUser } from "@supabase/auth-js";
+import { supabase } from "../lib/supabase";
+
+export type User = SupabaseUser & { userName: string };
+
+type SignupParams = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+export const authRepository = {
+  async signup({ name, email, password }: SignupParams): Promise<User | null> {
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: { data: { name } },
+    });
+    if (error != null) throw new Error(error.message);
+    if (data.user) {
+      return { ...data.user, userName: data.user.user_metadata?.name || "" };
+    } else {
+      return null;
+    }
+  },
+};
