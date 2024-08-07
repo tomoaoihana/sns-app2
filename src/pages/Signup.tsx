83b15118c1219cdf3aa6e2429004601b1ce88345
tomoaoihana <tamu.kusanishi@gmail.com>
2024-08-08 08:47:53 +0900

    会員登録画面の実装

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
+import { authRepository } from "../repositries/auth";
+
 function Signup() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signup = async () => {
+    const user = await authRepository.signup({ name, email, password });
+    console.log(user);
+  };
   return (
     <div className="min-h-screen bg-gray-100 py-10 px-4 sm:px-6 lg:px-8">
       <div className="flex flex-col items-center">
@@ -15,6 +26,7 @@ function Signup() {
                 </label>
                 <div className="mt-1">
                   <input
+                    onChange={(e) => setName(e.target.value)}
                     id="username"
                     name="username"
                     placeholder="ユーザー名"
@@ -33,6 +45,7 @@ function Signup() {
                 </label>
                 <div className="mt-1">
                   <input
+                    onChange={(e) => setEmail(e.target.value)}
                     id="email"
                     name="email"
                     placeholder="メールアドレス"
@@ -51,6 +64,7 @@ function Signup() {
                 </label>
                 <div className="mt-1">
                   <input
+                    onChange={(e) => setPassword(e.target.value)}
                     id="password"
                     name="password"
                     placeholder="パスワード"
@@ -61,7 +75,11 @@ function Signup() {
                 </div>
               </div>
               <div>
-                <button className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50 disabled:cursor-not-allowed">
+                <button
+                  onClick={signup}
+                  disabled={name === "" || email === "" || password === ""}
+                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50 disabled:cursor-not-allowed"
+                >
                   登録
                 </button>
               </div>
